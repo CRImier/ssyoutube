4888ccb4b67bd3424cb160e69d5086c1c1c90662
CRImier <crimier@yandex.ru>
2016-09-19 07:53:28 +0000

    Initial commit - works for me

---
+++ main.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+
+import q
+import os
+import web
+from threading import Thread
+from youtube_dl import YoutubeDL
+
+opts = {
+#'progress_hooks': [my_hook],
+"outtmpl":"%(title)s",
+"postprocessors":[{"key":'FFmpegExtractAudio', "preferredcodec":"mp3", "preferredquality":"0"}]
+}
+
+music_dir = "/media/16GB-Music/Music"
+
+class Favicon():
+    def GET(self):
+        return web.seeother('static/favicon.ico')
+
+class MainPage():
+    def GET(self):
+        if 'v' in web.input():
+            link = "http://youtube.com/watch?v={}".format(web.input()['v'])
+            q.queue.put(link)
+            print(link)
+            return web.seeother(link)
+        else:
+            return "Whoosh"
+        
+    def POST(self):
+        return "Woot woot"
+
+def dl_thread():
+    while True:
+        link = q.queue.get()
+        download(link)
+
+def download(link):
+    with YoutubeDL(opts) as ydl:
+        ydl.download([link])
+
+urls = ("/favicon.ico", "Favicon",
+"/.*", "MainPage")
+app = web.application(urls, globals())
+web.debug = False
+
+if __name__ == "__main__":
+    os.chdir(music_dir)
+    t = Thread(target=dl_thread)
+    t.daemon = True
+    t.start()
+    app.run()
---
+++ q.py
@@ -0,0 +1,5 @@
+import Queue #HAAAAAAAAAAAAAAXXX
+
+#web.py re-imports the main.py or something, what's why I make that queue reside in another module
+
+queue = Queue.Queue()
