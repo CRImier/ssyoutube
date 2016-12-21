4e549193348e86239b5f48feb4391dd69a274614
CRImier <crimier@yandex.ru>
2016-12-21 23:45:50 +0200

    Added an interface for custom filename filtering

--- ssyoutube.py
+++ ssyoutube.py
@@ -4,19 +4,26 @@ from __future__ import unicode_literals
 import file_queue
 import os
 import web
+import string
 from time import sleep
 from threading import Thread
 from youtube_dl import YoutubeDL
-
-def print_info(info_dict):
-    print(info_dict["title"])
+from youtube_dl.utils import sanitize_path, compat_str
 
 q = file_queue.FileQueue('videos_to_dl')
 
+allowed_chars = unicode(string.ascii_letters+string.digits+"!@#$%^&*()_+\|{}[]-_=+'\",.<>?:; ")
+
+class MyYoutubeDL(YoutubeDL):
+    def prepare_filename(self, info_dict):
+        filename = sanitize_path(info_dict["title"])
+        filename = filename.replace(u'\u3010', u'[').replace(u'\u3011', u']')
+        for char in filename[:]:
+            if char not in allowed_chars:
+                filename = filename.replace(char, u'')
+        return compat_str(filename)
+
 opts = {
-"restrictfilenames":True,
-"match_filter":print_info,
-"outtmpl":"%(title)s",
 "postprocessors":[{"key":'FFmpegExtractAudio', "preferredcodec":"mp3", "preferredquality":"0"}]
 }
 
@@ -50,7 +57,7 @@ def dl_thread():
             sleep(1)
 
 def download(link):
-    with YoutubeDL(opts) as ydl:
+    with MyYoutubeDL(opts) as ydl:
         ydl.download([link])
 
 urls = ("/favicon.ico", "Favicon",
